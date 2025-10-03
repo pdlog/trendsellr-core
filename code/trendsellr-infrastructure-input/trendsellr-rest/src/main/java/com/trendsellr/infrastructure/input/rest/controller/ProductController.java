d9e56c9c40a043181b13b1460e78051236a7e974
Pedro Daniel López González <pedrodlg1989@gmail.com>
2025-10-03 21:39:26 +0200

    refactor: Implement contract-first with OpenAPI and DTOs

--- code/trendsellr-infrastructure-input/trendsellr-rest/src/main/java/com/trendsellr/infrastructure/input/rest/controller/ProductController.java
+++ code/trendsellr-infrastructure-input/trendsellr-rest/src/main/java/com/trendsellr/infrastructure/input/rest/controller/ProductController.java
@@ -3,17 +3,17 @@ package com.trendsellr.infrastructure.input.rest.controller;
 import com.trendsellr.application.usecase.CreateProductUseCase;
 import com.trendsellr.application.usecase.FindAllProductsUseCase;
 import com.trendsellr.application.usecase.FindProductByIdUseCase;
+import com.trendsellr.domain.model.Product;
 import com.trendsellr.infrastructure.input.rest.api.ProductsApi;
 import com.trendsellr.infrastructure.input.rest.dto.ProductDTO;
 import com.trendsellr.infrastructure.input.rest.mapper.ProductDTOMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Slf4j
 @RestController
@@ -28,10 +28,10 @@ public class ProductController implements ProductsApi {
 
     @Override
     public ResponseEntity<ProductDTO> createProduct(ProductDTO productDto) {
-        log.info("REST request to create Product: {}", productDTO);
+        log.info("REST request to create Product: {}", productDto);
 
-        var productToCreate = this.productDtoMapper.toDomain(productDTO);
-        Product createdProduct = this.createProductUseCase.dispatch(product);
+        var productToCreate = this.productDtoMapper.toDomain(productDto);
+        Product createdProduct = this.createProductUseCase.dispatch(productToCreate);
 
         return new ResponseEntity<>(this.productDtoMapper.toDto(createdProduct), HttpStatus.CREATED);
     }
